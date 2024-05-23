149c1128511da89d8e89e2393edf2d6e5fcac3e9
İrem Bağçıvan <230601013@bakircay.edu.tr>
2024-05-23 19:59:53 +0300

    Class Structure
    A classroom structure was created for staff, doctors and nurses. Doctor and Nurse classes are inherited from Staff class.

---
+++ Doktor.py
@@ -0,0 +1,33 @@
+from Personel import Personel
+class Doktor(Personel):
+    def __init__(self, personel_no, ad, soyad, departman, maas, uzmanlik, deneyim_yili, hastane):
+        super().__init__(self, personel_no, ad, soyad, departman, maas)
+        self.__uzmanlik = uzmanlik
+        self.__deneyim_yili = deneyim_yili
+        self.__hastane = hastane
+    
+    def set_uzmanlik(self, uzmanlik):
+        self.__uzmanlik = uzmanlik
+    
+    def set_deneyim_yili(self, deneyim_yili):
+        self.__deneyim_yili = deneyim_yili
+    
+    def set_hastane(self, hastane):
+        self.__hastane = hastane
+    
+    def get_uzmanlik(self):
+        return self.__uzmanlik
+    
+    def get_deneyim_yili(self):
+        return self.__deneyim_yili
+    
+    def get_hastane(self):
+        return self.__hastane
+    
+    def __str__(self):
+        return super().__str__() + f", Uzmanlık: {self.__uzmanlik}, Deneyim Yılı: {self.__deneyim_yili}, 
+                                       Hastane: {self.__hastane}"
+
+    
+    
+
---
+++ Hemsire.py
@@ -0,0 +1,29 @@
+from Personel import Personel
+class Hemsire():
+    def __init__(self, personel_no, ad, soyad, departman, maas, calisma_saati, sertifika, hastane):
+        super().__init__(self,personel_no, ad, soyad, departman, maas)
+        self.__calisma_saati = calisma_saati
+        self.__sertifika = sertifika
+        self.__hastane = hastane
+
+    def set_calisma_saati(self, calisma_saati):
+        self.__calisma_saati = calisma_saati
+    
+    def set_sertifika(self, sertifika):
+        self.__sertifika = sertifika
+    
+    def set_hastane(self, hastane):
+        self.__hastane = hastane
+
+    def get_calisma_saati(self):
+        return self.__calisma_saati
+    
+    def get_sertifika(self):
+        return self.__sertifika
+    
+    def get_hastane(self):
+        return self.__hastane
+    
+    def __str__(self):
+        return super().__str__() + f", Çalışma Saati: {self.__calisma_saati}, Sertifika: {self.__sertifika}, 
+                                       Hastane: {self.__hastane}"
